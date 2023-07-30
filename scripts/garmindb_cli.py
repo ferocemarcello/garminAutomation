3133596c0f7095a57a078943fe194fb0fe9c620b
Marcello Feroce <ferocemarcello@gmail.com>
2023-07-30 21:19:25 +0200

    refactoring

--- scripts/garmindb_cli.py
+++ scripts/garmindb_cli.py
@@ -10,7 +10,6 @@ __author__ = "Tom Goetz"
 __copyright__ = "Copyright Tom Goetz"
 __license__ = "GPL"
 
-import argparse
 import datetime
 import logging
 import sys
@@ -18,7 +17,6 @@ import sys
 from garmindb import ConfigManager, GarminConnectConfigManager, PluginManager
 from garmindb import Download, Copy
 from garmindb import Statistics
-from garmindb import python_version_check, format_version
 from garmindb.garmindb import GarminDb, Sleep, Weight, RestingHeartRate, MonitoringDb, MonitoringHeartRate, \
     ActivitiesDb, GarminSummaryDb
 from garmindb.summarydb import SummaryDb
@@ -63,10 +61,10 @@ def __get_date_and_days(db, latest, table, col, stat_name):
     if date is None or days is None:
         logger.error("Missing config: need %s_start_date and download_days. Edit GarminConnectConfig.py.", stat_name)
         sys.exit()
-    return (date, days)
+    return date, days
 
 
-def copy_data(overwite, latest, stats):
+def copy_data(latest, stats):
     """Copy data from a mounted Garmin USB device to files."""
     logger.info("___Copying Data___")
     copy = Copy(gc_config.device_mount_dir())
@@ -138,56 +136,8 @@ def download_data():
         download.get_rhr(rhr_dir, date, days, False)
         root_logger.info("Saved rhr files for %s (%d) to %s for processing", date, days, rhr_dir)
 
+
 def main():
-    """Manage Garmin device data."""
-    python_version_check(sys.argv[0])
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--version", help="print the program's version", action='version',
-                        version=format_version(sys.argv[0]))
-    parser.add_argument("-t", "--trace", help="Turn on debug tracing", type=int, default=0)
-    modes_group = parser.add_argument_group('Modes')
-    modes_group.add_argument("-b", "--backup", help="Backup the database files.", dest='backup_dbs',
-                             action="store_true", default=False)
-    modes_group.add_argument("-d", "--download", help="Download data from Garmin Connect for the chosen stats.",
-                             dest='download_data', action="store_true", default=False)
-    modes_group.add_argument("-c", "--copy", help="copy data from a connected device", dest='copy_data',
-                             action="store_true", default=False)
-    modes_group.add_argument("-i", "--import", help="Import data for the chosen stats", dest='import_data',
-                             action="store_true", default=False)
-    modes_group.add_argument("--analyze", help="Analyze data in the db and create summary and derived tables.",
-                             dest='analyze_data', action="store_true", default=False)
-    modes_group.add_argument("--rebuild_db", help="Delete Garmin DB db files and rebuild the database.",
-                             action="store_true", default=False)
-    modes_group.add_argument("--delete_db", help="Delete Garmin DB db files for the selected activities.",
-                             action="store_true", default=False)
-    modes_group.add_argument("-e", "--export-activity",
-                             help="Export an activity to a TCX file based on the activity\'s id", type=int)
-    modes_group.add_argument("--basecamp-activity", help="Export an activity to Garmin BaseCamp", type=int)
-    modes_group.add_argument("-g", "--google-earth-activity", help="Export an activity to Google Earth", type=int)
-    # stat types to operate on
-    stats_group = parser.add_argument_group('Statistics')
-    stats_group.add_argument("-A", "--all", help="Download and/or import data for all enabled stats.",
-                             action='store_const', dest='stats',
-                             const=gc_config.enabled_stats(), default=[])
-    stats_group.add_argument("-a", "--activities", help="Download and/or import activities data.", dest='stats',
-                             action='append_const', const=Statistics.activities)
-    stats_group.add_argument("-m", "--monitoring", help="Download and/or import monitoring data.", dest='stats',
-                             action='append_const', const=Statistics.monitoring)
-    stats_group.add_argument("-r", "--rhr", help="Download and/or import resting heart rate data.", dest='stats',
-                             action='append_const', const=Statistics.rhr)
-    stats_group.add_argument("-s", "--sleep", help="Download and/or import sleep data.", dest='stats',
-                             action='append_const', const=Statistics.sleep)
-    stats_group.add_argument("-w", "--weight", help="Download and/or import weight data.", dest='stats',
-                             action='append_const', const=Statistics.weight)
-    modifiers_group = parser.add_argument_group('Modifiers')
-    modifiers_group.add_argument("-l", "--latest", help="Only download and/or import the latest data.",
-                                 action="store_true", default=False)
-    modifiers_group.add_argument("-o", "--overwrite",
-                                 help="Overwite existing files when downloading. The default is to only download missing files.",
-                                 action="store_true", default=False)
-    args = parser.parse_args()
-
     download_data()
 
 
