e2b4044244f4682bc6e5412e38b0e058c79cc39b
Marcello Feroce <ferocemarcello@gmail.com>
2023-08-05 08:00:05 +0200

    refactoring

--- scripts/rest_client_pers.py
+++ scripts/rest_client_pers.py
@@ -1,4 +1,3 @@
-import logging
 import enum
 
 
@@ -9,11 +8,12 @@ class RestProtocol(enum.Enum):
     https = 'https'
 
 
-class RestClient():
-    """Class that encapsilates REST functionality for a single API endpoint."""
+class RestClientPers:
+    """Class that encapsulates REST functionality for a single API endpoint."""
 
     agents = {
-        'Chrome_Linux': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/1337 Safari/537.36',
+        'Chrome_Linux': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/1337 '
+                        'Safari/537.36',
         'Firefox_MacOS': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0'
     }
     agent = agents['Firefox_MacOS']
@@ -24,8 +24,10 @@ class RestClient():
     }
 
     def __init__(self, session, host, base_route, protocol=RestProtocol.https, port=443, headers=None,
-                 additional_headers={}):
+                 additional_headers=None):
         """Return a new RestClient instance given a requests session and the base URL of the API."""
+        if additional_headers is None:
+            additional_headers = {}
         self.session = session
         self.host = host
         self.protocol = protocol
@@ -39,10 +41,11 @@ class RestClient():
 
     @classmethod
     def inherit(cls, rest_client, route):
-        """Create a new RestClient object from a RestClient object. The new object will handle an API endpoint that is a child of the old RestClient."""
-        return RestClient(session=rest_client.session, host=rest_client.host,
-                          base_route=f'{rest_client.base_route}/{route}',
-                          protocol=rest_client.protocol, port=rest_client.port, headers=rest_client.headers)
+        """Create a new RestClient object from a RestClient object. The new object will handle an API endpoint that
+        is a child of the old RestClient."""
+        return RestClientPers(session=rest_client.session, host=rest_client.host,
+                              base_route=f'{rest_client.base_route}/{route}',
+                              protocol=rest_client.protocol, port=rest_client.port, headers=rest_client.headers)
 
     def compose_url(self, leaf_route=None):
         """Return the url for the REST endpoint including leaf if supplied."""
@@ -55,8 +58,12 @@ class RestClient():
             return f'{self.protocol.name}://{self.host}/{path}'
         return f'{self.protocol.name}://{self.host}:{self.port}/{path}'
 
-    def get(self, leaf_route, additional_headers={}, params={}):
+    def get(self, leaf_route, additional_headers=None, params=None):
         """Make a REST API call using the GET method."""
+        if params is None:
+            params = {}
+        if additional_headers is None:
+            additional_headers = {}
         total_headers = self.headers.copy()
         total_headers.update(additional_headers)
         response = self.session.get(self.compose_url(leaf_route), headers=total_headers, params=params)
