e1933f026be11c30b1864f5f1c837e63cfeb7c46
Marcello Feroce <ferocemarcello@gmail.com>
2023-08-06 16:11:23 +0200

    refactoring

--- garmindb_cli.py
+++ garmindb_cli.py
@@ -12,11 +12,9 @@ def download_data(downloader: Download, start_date: datetime.date, end_date: dat
     activities = downloader.get_activities(start_date=start_date, end_date=end_date)
     daily_summaries = downloader.get_daily_stats(start_date=start_date, end_date=end_date,
                                                  url_param_function=downloader.url_param_summary_day)
-    hydration_days = downloader.get_daily_stats(start_date=start_date, end_date=end_date,
+    '''hydration_days = downloader.get_daily_stats(start_date=start_date, end_date=end_date,
                                                 url_param_function=downloader.url_param_hydration_day)
-    # monitoring_days = downloader.get_daily_stats(date=start_date, days=count_days,
-    # stat_function=downloader.get_monitoring_day)
-
+    '''
     sleep_days = downloader.get_daily_stats(start_date=start_date, end_date=end_date,
                                             url_param_function=downloader.url_param_sleep_day)
 
@@ -25,10 +23,24 @@ def download_data(downloader: Download, start_date: datetime.date, end_date: dat
 
     rhr_days = downloader.get_daily_stats(start_date=start_date, end_date=end_date,
                                           url_param_function=downloader.url_param_rhr_day)
-    date_dict = {}
-    return {"activity_types": activity_types, "activities": activities, "daily_summaries": daily_summaries,
-            "hydration_days": hydration_days, "sleep_days": sleep_days, "weight_days": weight_days,
-            "rhr_days": rhr_days}
+    date_dict = dict()
+    delta = end_date - start_date
+    for day in range(delta.days + 1):
+        index_date = start_date + datetime.timedelta(days=day)
+        date_dict[index_date] = dict()
+        date_dict[index_date]["activities"] = \
+            [item for item in activities.items()
+             if datetime.datetime.strptime(item[1]["summaryDTO"]['startTimeLocal'],
+                                           '%Y-%m-%dT%H:%M:%S.%f').date().__eq__(index_date)]
+        date_dict[index_date]["summaries"] = next(item for item in daily_summaries if item["calendarDate"] ==
+                                                  index_date.isoformat())
+        date_dict[index_date]["sleep"] = next(item for item in sleep_days if item['dailySleepDTO']["calendarDate"] ==
+                                              index_date.isoformat())
+        date_dict[index_date]["weight"] = next(item for item in weight_days if item["startDate"] ==
+                                               index_date.isoformat())
+        date_dict[index_date]["rhr"] = next(item for item in rhr_days if item["statisticsStartDate"] ==
+                                            index_date.isoformat())
+    return date_dict
 
 
 def main(username=None, password=None, start_date: str = None, end_date: str = None, google_sheet_file: str = None):
@@ -50,4 +62,5 @@ if __name__ == "__main__":
     start_date_input = sys.argv[3]
     end_date_input = sys.argv[4]
     google_sheet_file_input = sys.argv[5]
-    main(username=username_input, password=password_input, start_date=start_date_input, end_date=end_date_input, google_sheet_file=google_sheet_file_input)
+    main(username=username_input, password=password_input, start_date=start_date_input, end_date=end_date_input,
+         google_sheet_file=google_sheet_file_input)
