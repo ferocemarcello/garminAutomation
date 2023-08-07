4adb471dd53d5526c38d18ddc73222cd7345eb09
Marcello Feroce <ferocemarcello@gmail.com>
2023-08-07 14:40:23 +0200

    refactoring

--- google_sheet_oauth.py
+++ google_sheet_oauth.py
@@ -10,11 +10,10 @@ class GoogleOauth:
         'https://www.googleapis.com/auth/spreadsheets',
         'https://www.googleapis.com/auth/drive'
     ]
-    credentials = json.load(open("credentials/client_secret_732661881343-tfkhdb1c7ov6ns6lnvhdoo4gbias077a.apps.googleusercontent.com.json"))
-    gc, authorized_user = gspread.oauth_from_dict(credentials)
+    credentials = json.load(open("credentials/client_secret_oauth_desktop.json"))
 
     def __init__(self):
-        self.client = gspread.authorize(credentials=GoogleOauth.credentials)
+        self.client, authorized_user = gspread.oauth_from_dict(GoogleOauth.credentials)
 
     def get_all_values_sheet(self, sheet_id: str):
         return self.client.open_by_key(sheet_id).get_worksheet(0).get_all_values()
