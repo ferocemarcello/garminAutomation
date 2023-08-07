b88ea6df14502d9e68e82f99d2289f2d0aa815c5
Marcello Feroce <ferocemarcello@gmail.com>
2023-08-07 12:30:12 +0200

    refactoring

--- google_sheet.py
+++ google_sheet.py
@@ -1,9 +1,18 @@
+import gspread
+
+
 class GoogleSheet:
-    def __init__(self, google_sheet_file: str):
-        self.file = google_sheet_file
+    def __init__(self, google_sheet_file: str, credentials_file: str):
+        self.sheet_id = google_sheet_file
+        self.client = gspread.create_from_service_account_file(credentials_file)
 
     def write_dates(self, start_date, end_date):
         pass
+
+    def get_all_values(self):
+        sheet = self.client.open_by_id(self.sheet_id)
+        # Get the data from the sheet
+        data = sheet.get_all_values()
 
     def fill_dates(self, interval_data):
         print(interval_data)
