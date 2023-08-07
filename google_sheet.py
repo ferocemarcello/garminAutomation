104415cef7896a314c8cab3236c388fa6d7ffaa1
Marcello Feroce <ferocemarcello@gmail.com>
2023-08-07 13:00:51 +0200

    refactoring

--- google_sheet.py
+++ google_sheet.py
@@ -1,18 +1,18 @@
 import gspread
 
 
-class GoogleSheet:
-    def __init__(self, google_sheet_file: str, credentials_file: str):
-        self.sheet_id = google_sheet_file
+class GoogleClient:
+    def __init__(self: str, credentials_file: str):
         self.client = gspread.service_account(filename=credentials_file)
 
     def write_dates(self, start_date, end_date):
         pass
 
-    def get_all_values(self):
-        sheet = self.client.open_by_id(self.sheet_id)
+    def get_all_values_sheet(self, sheet_id:str):
+        sheet = self.client.open_by_key(sheet_id)
         # Get the data from the sheet
         data = sheet.get_all_values()
+        return data
 
     def fill_dates(self, interval_data):
         print(interval_data)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import datetime
 import os
 import sys
 from download import Download
-from google_sheet import GoogleSheet
+from google_sheet import GoogleClient
 
 
 def download_data(downloader: Download, start_date: datetime.date, end_date: datetime.date):
@@ -47,9 +47,8 @@ def download_data(downloader: Download, start_date: datetime.date, end_date: dat
 def main(username=None, password=None, start_date: str = None, end_date: str = None, google_sheet_file: str = None):
     if None in [username, password, start_date, end_date, google_sheet_file]:
         sys.exit()
-    g_sheet = GoogleSheet(google_sheet_file=google_sheet_file, credentials_file="credentials"
-                                                                                "/garminautomation_credentials.json")
-    g_sheet.write_dates(start_date, end_date)
+    g_sheet = GoogleClient(credentials_file="credentials/garminautomation_credentials.json")
+    sheet_values = g_sheet.get_all_values_sheet(sheet_id=google_sheet_file)
     download_instance = Download()
     if not download_instance.login(username, password):
         sys.exit()
